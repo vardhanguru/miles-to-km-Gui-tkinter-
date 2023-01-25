d70e1862d21f278c1ec96be9163066acffe2feec
vardhanguru <68608658+vardhanguru@users.noreply.github.com>
2023-01-25 08:04:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,23 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import tkinter
 window=tkinter.Tk()
+#settingwindow size to 300px, 150px
 window.title("Miles to Km converter")
 window.minsize(width=300,height=150)
 
 
+#label for to display the value in KM
 my_label=tkinter.Label(text="0")
 my_label.place(x=80,y=40)
 
+#text for understanding purpose
 texts=tkinter.Label(text="is eual to ")
 texts.place(x=20,y=40)
 
+#miles value
 miles=tkinter.Label(text="Miles")
 miles.place(x=20,y=20)
+
 def on_click():
     my_label['text']=str(1.60934*int(entry.get()))+' kms'
 button=tkinter.Button(text="click me",borderwidth=1,command=on_click)
